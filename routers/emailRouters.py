6b09a066ef2082598d79a133006f5e69cbb31d4b
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-06-09 11:27:53 -0400

    response body | documentation

--- routers/emailRouters.py
+++ routers/emailRouters.py
@@ -3,10 +3,15 @@ import json
 
 # fastApi
 from fastapi import APIRouter
+from fastapi import status
 from fastapi.responses import JSONResponse
+from fastapi import HTTPException
 
 # services
-from services.emailsServices import Inbox, inbox_schema
+from services.emailsServices import Inbox
+
+# models
+from models.emailModels import ApiSES
 
 emailRouter = APIRouter()
 
@@ -17,9 +22,49 @@ emails = []
     tags=['Emails']
 )
 def post_send_email_for_user(inbox: Inbox) -> dict:
+    """
+    # Params:
+
+    id:uuid
+    name:str
+    email:str[email]
+    number_phone:str[validate numbers]
+        Example: 4120000000
+    
+    Example request Success:
+    
+    ```
+    {
+    "id": "6312cfbd-dc47-46ed-84d9-20c03aba7402",
+    "name": "string",
+    "email": "user@example.com",
+    "number_phone": "4120000000"
+    }
+    ```
+
+    Example request err:
+
+    ```
+    {
+    "id": "6312cfbd-dc47-46ed-84d9-20c03aba7402",
+    "name": "string",
+    "email": "user@example.com",
+    "number_phone": "+58-4120000000"
+    }
+    ```
+    """
 
     item = json.loads(inbox.json())
-    emails.append(item)
+    ses = ApiSES(body=inbox).send_email()
 
-    print(inbox_schema)
-    return JSONResponse(status_code=200, content={"message": "success", "data": item})
+    if ses['status'] == False:
+        response = {'message': f'{ses["response"]}' ,'HttpResponse': f'{status.HTTP_404_NOT_FOUND}'}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=response
+        )
+    else:
+        item['MessageId'] = ses['response']
+        emails.append(item)
+    
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "success", "data": item})
