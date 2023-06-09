c0f1350228ffd009d1747e44cc83d2b34d76574a
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-06-09 10:07:26 -0400

    generate json schema

---
+++ jsonSchemas/generateJsonSchema/generateJsonSchema.py
@@ -0,0 +1,22 @@
+# python
+import os
+import json
+
+# dotenv
+from dotenv import load_dotenv
+
+# services
+from services.emailsServices import inbox_schema
+
+load_dotenv()
+
+JSON_SCHEMA_URL = os.getenv('JSON_SCHEMA_URL')
+PATH_SAVE = 'jsonSchemas/schemas'
+
+inbox_schema['$schema'] = JSON_SCHEMA_URL
+
+if not os.path.exists(PATH_SAVE):
+    os.makedirs(PATH_SAVE)
+
+with open(PATH_SAVE + f'/{str( inbox_schema["title"]).lower() }.json', 'w', encoding='utf-8') as f:
+    json.dump(inbox_schema, f, ensure_ascii=False, indent=4)
